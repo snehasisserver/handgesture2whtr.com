209a14ffc2eb2056cf3a3f4f7c301c1d18f94f65
Snehasis Ghosh <91667746+snehasisserver@users.noreply.github.com>
2022-06-09 11:18:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+Webcam.set({
+    width:400,
+    height:350,
+    image_format:'png',
+   png_quality:90
+});
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapsort(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+console.log("ml5 version : ", ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v4mzcxJXd/model.json',modelLoaded);
+function modelLoaded(){
+    console.log("Model Loaded");
+
+}
